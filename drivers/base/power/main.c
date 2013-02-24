815eeb69e0b184f26ceec01105d7482c93756223
halaszk <halaszk@deathlord.eu>
2013-02-24 12:55:31 +0100

    PM: Prevent runtime suspend during system resume
    This patch (as1591) moves the pm_runtime_get_noresume() and
    pm_runtime_put_sync() calls from __device_suspend() and
    device_resume() to device_prepare() and device_complete() in the PM
    core.
    
    The reason for doing this is to make sure that parent devices remain
    at full power (i.e., don't go into runtime suspend) while their
    children are being resumed from a system sleep.
    
    The PCI core already contained equivalent code to serve the same
    purpose.  The patch removes the duplicated code, since it is no longer
    needed.  One of the comments from the PCI core gets moved into the PM
    core, and a second comment is added to explain whe the _get_noresume
    and _put_sync calls are present.
    
    Signed-off-by: Alan Stern <stern@rowland.harvard.edu>
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -691,6 +691,7 @@ static void device_complete(struct device *dev, pm_message_t state)
 	}
 
 	device_unlock(dev);
+	pm_runtime_put_sync(dev);
 }
 
 /**
@@ -1026,6 +1027,8 @@ static int device_prepare(struct device *dev, pm_message_t state)
 {
 	int error = 0;
 
+	pm_runtime_get_noresume(dev);
+
 	device_lock(dev);
 
 	if (dev->pwr_domain) {
@@ -1058,6 +1061,13 @@ static int device_prepare(struct device *dev, pm_message_t state)
 
  End:
 	device_unlock(dev);
+
+	/*
+	* If failed prepare, should allow runtime suspend again because
+	* the complete phase of this device is never invoked
+	*/
+	if (error)
+	pm_runtime_put_sync(dev);
 
 	return error;
 }
