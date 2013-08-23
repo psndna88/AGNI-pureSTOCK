522c0298e49b086bfa1032a9bae2ec9e81127f29
psndna88 <psndna88@gmail.com>
2013-08-23 18:54:44 +0530

    PVR SGX540: Kernel Module sources DDK v1.9@2291151

--- build_kernel.sh
+++ build_kernel.sh
@@ -9,21 +9,33 @@ export ARCH=arm
 
 if [ ! -f $KERNELDIR/.config ];
 then
-  make defconfig psn_p31xx_oc_v2.2_defconfig
+  make defconfig psn_p31xx_oc_v2.3_defconfig
 fi
 
 . $KERNELDIR/.config
 
+echo "BEGINING KERNEL COMPILATION .........."
 cd $KERNELDIR/
 make -j3 || exit 1
 
-mkdir -p $KERNELDIR/BUILT/lib/modules
+mkdir -p $KERNELDIR/BUILT-P31xx/lib/modules
+rm $KERNELDIR/BUILT-P31xx/lib/modules/*
+rm $KERNELDIR/BUILT-P31xx/zImage
 
-rm $KERNELDIR/BUILT/lib/modules/*
-rm $KERNELDIR/BUILT/zImage
+echo "BEGINING SGX540 PVR KM COMPILATION ..........."
+cd $KERNELDIR/pvr_source/eurasiacon/build/linux2/omap4430_android
+make clean
+make TARGET_PRODUCT="blaze_tablet" BUILD=release TARGET_SGX=540 PLATFORM_VERSION=4.1.2 || exit
+make clean
+mv $KERNELDIR/pvr_source/eurasiacon/binary2_540_120_omap4430_android_release/target/*.ko $KERNELDIR/BUILT-P31xx/lib/modules/
+rm -rf $KERNELDIR/pvr_source/eurasiacon/binary2_540_120_omap4430_android_release
 
-find -name '*.ko' -exec cp -av {} $KERNELDIR/BUILT/lib/modules/ \;
-${CROSS_COMPILE}strip --strip-unneeded $KERNELDIR/BUILT/lib/modules/*
-cp $KERNELDIR/arch/arm/boot/zImage $KERNELDIR/BUILT/
+echo "PREPARING BUILT-P31xx ..........."
+cd $KERNELDIR
+find -name '*.ko' -exec cp -av {} $KERNELDIR/BUILT-P31xx/lib/modules/ \;
+${CROSS_COMPILE}strip --strip-unneeded $KERNELDIR/BUILT-P31xx/lib/modules/*
+cp $KERNELDIR/arch/arm/boot/zImage $KERNELDIR/BUILT-P31xx/
 
 mv .git-halt .git
+
+echo "COMPILATION TASKS FOR STOCK P31xx COMPLETE !!!!!!!!"
