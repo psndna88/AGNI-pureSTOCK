58e3f739712a85818d51c795603996a59619695a
psndna88 <psndna88@gmail.com>
2013-09-28 20:24:29 +0530

    Revert "Implemented printk: Add sys kernel interface to configure linux printk logging"
    This reverts commit 2b609d26b74eca846accc37224e47c6628e9b714.

--- kernel/printk.c
+++ kernel/printk.c
@@ -41,7 +41,6 @@
 #include <linux/cpu.h>
 #include <linux/notifier.h>
 #include <linux/rculist.h>
-#include "printk_interface.h"
 
 #include <asm/uaccess.h>
 #include <mach/sec_debug.h>
@@ -844,12 +843,6 @@ asmlinkage int printk(const char *fmt, ...)
 	va_list args;
 	int r;
 
-// if printk mode is disabled, terminate instantly
-    	if (printk_mode == 0)
-    	{
-        	return 0;
-    	}
-
 #ifdef CONFIG_KGDB_KDB
 	if (unlikely(kdb_trap_printk)) {
 		va_start(args, fmt);
@@ -944,12 +937,6 @@ asmlinkage int vprintk(const char *fmt, va_list args)
 	char *p;
 	size_t plen;
 	char special;
-
-// if printk mode is disabled, terminate instantly
-   	if (printk_mode == 0)
-        {
-                return 0;
-        }
 
 	boot_delay_msec();
 	printk_delay();
