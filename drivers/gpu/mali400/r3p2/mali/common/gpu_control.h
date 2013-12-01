350f54842b5b504988c2629d7e6210dadb8d89ab
psndna88 <psndna88@gmail.com>
2013-12-01 12:45:39 +0530

    Implemented MALI r3p2 GPU OC/UV (Derteufel)

---
+++ drivers/gpu/mali400/r3p2/mali/common/gpu_control.h
@@ -0,0 +1,15 @@
+/*
+ * gpu_control.h -- a control interface for the sgs3 and note2
+ *
+ *  Copyright (C) 2011 Michael Wodkins
+ *  twitter - @xdanetarchy
+ *  XDA-developers - netarchy
+ *
+ *  This program is free software; you can redistribute  it and/or modify it
+ *  under  the terms of the GNU General Public License as published by the
+ *  Free Software Foundation;
+ *
+ */
+#include "../platform/pegasus-m400/exynos4_pmm.h"
+
+void gpu_control_start(void);
