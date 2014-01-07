372e9894c1d23b048abc7858c3491ac7ff765949
arter97 <qkrwngud825@gmail.com>
2014-01-07 22:10:39 +0900

    ashmem: fix a typo introduced by Samsung (DerTeufel)
    Including Google's official mach-msm Linux kernel,
    lots of Android kernels are returning -1 here.
    
    It should return -1 because it's the appropriate value
    when locking mutex has failed.
    
    Also, the initial ashmem writer, Robert Love also committed code returning -1.
    
    We predict that Samsung has made this typo because
    the upper code, "if (!sc->nr_to_scan)" returns lru_count.
    
    Credit goes to DerTeufel for finding this typo.

--- mm/ashmem.c
+++ mm/ashmem.c
@@ -358,7 +358,7 @@ static int ashmem_shrink(struct shrinker *s, struct shrink_control *sc)
 		return lru_count;
 
 	if (!mutex_trylock(&ashmem_mutex))
-		return lru_count;
+		return -1;
 	list_for_each_entry_safe(range, next, &ashmem_lru_list, lru) {
 		struct inode *inode = range->asma->file->f_dentry->d_inode;
 		loff_t start = range->pgstart * PAGE_SIZE;
