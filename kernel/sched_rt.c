bd6c519dcb3c7bf0df2ac0ccafa924a0e6173c07
psndna88 <psndna88@gmail.com>
2014-01-07 23:16:37 +0530

    cpu boost: backport missing functions from 3.4 (DerTeufel) reference: http://webcache.googleusercontent.com/search?q=cache:7y4Vnjzxo6wJ:androidfilesgit.org/MaC/android_kernel_jewel/commit/8e9f9bdc0bb75222891cb39389fef6910dcb085d.diff+&cd=1&hl=en&ct=clnk&gl=il
    thx to dorimanx for commiting cpu boost to his 3.12 kernel. i would have missed this driver without him
    
    Change-Id: I3cd786129f7eff14bb775194919c803566e07f25 (reverted from commit 1be7bb8d895ae518fd9459614ec7848802278ddd)

--- kernel/sched_rt.c
+++ kernel/sched_rt.c
@@ -1383,6 +1383,8 @@ static int push_rt_task(struct rq *rq)
 	struct task_struct *next_task;
 	struct rq *lowest_rq;
 
+	bool moved = false;
+
 	if (!rq->rt.overloaded)
 		return 0;
 
@@ -1452,6 +1454,7 @@ retry:
 
 	deactivate_task(rq, next_task, 0);
 	set_task_cpu(next_task, lowest_rq->cpu);
+	moved = true;
 	activate_task(lowest_rq, next_task, 0);
 
 	resched_task(lowest_rq->curr);
@@ -1460,6 +1463,11 @@ retry:
 
 out:
 	put_task_struct(next_task);
+
+  	if (moved && task_notify_on_migrate(next_task))
+    		atomic_notifier_call_chain(&migration_notifier_head,
+             	cpu_of(lowest_rq),
+             	(void *)cpu_of(rq));
 
 	return 1;
 }
@@ -1474,8 +1482,11 @@ static void push_rt_tasks(struct rq *rq)
 static int pull_rt_task(struct rq *this_rq)
 {
 	int this_cpu = this_rq->cpu, ret = 0, cpu;
-	struct task_struct *p;
+	struct task_struct *p = NULL;
 	struct rq *src_rq;
+
+	bool moved = false;
+	int src_cpu = 0;
 
 	if (likely(!rt_overloaded(this_rq)))
 		return 0;
@@ -1536,6 +1547,10 @@ static int pull_rt_task(struct rq *this_rq)
 			deactivate_task(src_rq, p, 0);
 			set_task_cpu(p, this_cpu);
 			activate_task(this_rq, p, 0);
+
+			moved = true;
+			src_cpu = cpu_of(src_rq);
+
 			/*
 			 * We continue with the search, just in
 			 * case there's an even higher prio task
@@ -1546,6 +1561,11 @@ static int pull_rt_task(struct rq *this_rq)
 skip:
 		double_unlock_balance(this_rq, src_rq);
 	}
+
+	if (moved && task_notify_on_migrate(p))
+		atomic_notifier_call_chain(&migration_notifier_head,
+					   this_cpu,
+					   (void *)src_cpu);
 
 	return ret;
 }
