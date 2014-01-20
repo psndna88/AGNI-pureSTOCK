77a6ae713888786070d53069d3bc141e657b683f
DerTeufel <dominik-kassel@gmx.de>
2014-01-20 20:54:25 +0100

    add f2fs as filesystem to async fsync
    Change-Id: I7078c716420e6c57866660b654594468a14af160

--- fs/namespace.c
+++ fs/namespace.c
@@ -2032,11 +2032,13 @@ static int do_new_mount(struct path *path, char *type, int flags,
 	if (err)
 		mntput(mnt);
 #ifdef CONFIG_ASYNC_FSYNC
-  if (!err && ((!strcmp(type, "ext4") &&
-      !strcmp(path->dentry->d_name.name, "data")) ||
-      (!strcmp(type, "fuse") &&
-      !strcmp(path->dentry->d_name.name, "emulated"))))
-                mnt->mnt_sb->fsync_flags |= FLAG_ASYNC_FSYNC;
+	if (!err && ((((!strcmp(type, "ext4")) || (!strcmp(type, "f2fs"))) &&
+		!strcmp(path->dentry->d_name.name, "data")) ||
+		(!strcmp(type, "fuse") &&
+		!strcmp(path->dentry->d_name.name, "emulated")))) {
+			mnt->mnt_sb->fsync_flags |= FLAG_ASYNC_FSYNC;
+			pr_info("namespace: enabled FLAG_ASYNC_FSYNC\n");
+		}
 #endif
 	return err;
 }
