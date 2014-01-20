f4a3676b1985979f1eb019d5d34222fd6a03da00
psndna88 <psndna88@gmail.com>
2014-01-20 18:08:24 +0530

    Revert "mdnie: Switchable sharpness fix (V2)"
    This reverts commit 8c4d088eeeadc9bd35f3ac8fd72373cc23d769aa.

--- drivers/video/samsung/mdnie.c
+++ drivers/video/samsung/mdnie.c
@@ -49,7 +49,6 @@
 #include "mdnie_table_superior.h"
 #elif defined(CONFIG_FB_S5P_S6E8AA0)
 #include "mdnie_table_m0.h"
-#include "mdnie_table_m0_sharpness_tweak.h"
 #elif defined(CONFIG_FB_S5P_EA8061) || defined(CONFIG_FB_S5P_S6EVR02)
 #include "mdnie_table_t0.h"
 #elif defined(CONFIG_FB_S5P_S6E63M0)
@@ -126,8 +125,6 @@
 static struct class *mdnie_class;
 struct mdnie_info *g_mdnie;
 
-int mdnie_preset = 0;
-
 static int mdnie_send_sequence(struct mdnie_info *mdnie, const unsigned short *seq)
 {
 	int ret = 0, i = 0;
@@ -174,12 +171,7 @@ static struct mdnie_tuning_info *mdnie_request_table(struct mdnie_info *mdnie)
 		table = &color_tone_table[mdnie->scenario % COLOR_TONE_1];
 		goto exit;
 	} else if (mdnie->scenario < SCENARIO_MAX) {
-		// depending on sharpness tweak status, take either normal or
-		// tweaked tuning table
-		if (mdnie_preset == 0)
-			table = &tuning_table[mdnie->cabc][mdnie->mode][mdnie->scenario];
-		else
-			table = &tuning_table_sharp_tweak[mdnie->cabc][mdnie->mode][mdnie->scenario];
+		table = &tuning_table[mdnie->cabc][mdnie->mode][mdnie->scenario];
 		goto exit;
 	}
 
@@ -207,7 +199,7 @@ exit:
 	return;
 }
 
-void mdnie_update(struct mdnie_info *mdnie)
+static void mdnie_update(struct mdnie_info *mdnie)
 {
 	struct mdnie_tuning_info *table = NULL;
 
