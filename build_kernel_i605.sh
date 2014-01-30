ae4afa13ee823bc9f0754147e4a739502eaaf0d2
psndna88 <psndna88@gmail.com>
2014-01-30 12:52:10 +0530

    Merge branch 'AGNI-pureSTOCK/pureSTOCK-I605-4.3'

---
+++ build_kernel_i605.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+export KERNELDIR=`readlink -f .`
+. ~/AGNi_stamp_STOCK.sh
+. ~/gcc_4.7.2_armv7l.sh
+
+export ARCH=arm
+
+if [ ! -f $KERNELDIR/.config ];
+then
+  make defconfig psn_t0ltevzw_v3.9.1_defconfig
+fi
+
+. $KERNELDIR/.config
+
+mv .git .git-halt
+
+cd $KERNELDIR/
+make -j2 || exit 1
+
+mkdir -p $KERNELDIR/BUILT/lib/modules
+
+rm $KERNELDIR/BUILT/lib/modules/*
+rm $KERNELDIR/BUILT/zImage
+
+find -name '*.ko' -exec cp -av {} $KERNELDIR/BUILT/lib/modules/ \;
+${CROSS_COMPILE}strip --strip-unneeded $KERNELDIR/BUILT/lib/modules/*
+cp $KERNELDIR/arch/arm/boot/zImage $KERNELDIR/BUILT/
+
+mv .git-halt .git
