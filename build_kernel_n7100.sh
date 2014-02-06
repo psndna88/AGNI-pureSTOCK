d47cb4f77a48d89734d6f9bdc588c2cb3b059888
psndna88 <psndna88@gmail.com>
2014-02-06 21:22:29 +0530

    Added v3.9.3 defconfig and updated build script

--- build_kernel_n7100.sh
+++ build_kernel_n7100.sh
@@ -7,7 +7,7 @@ export ARCH=arm
 
 if [ ! -f $KERNELDIR/.config ];
 then
-  make defconfig psn_n7100_v3.9.1_defconfig
+  make defconfig psn_n7100_v3.9.3_defconfig
 fi
 
 . $KERNELDIR/.config
@@ -17,13 +17,13 @@ mv .git .git-halt
 cd $KERNELDIR/
 make -j2 || exit 1
 
-mkdir -p $KERNELDIR/BUILT/lib/modules
+mkdir -p $KERNELDIR/BUILT_N7100/lib/modules
 
-rm $KERNELDIR/BUILT/lib/modules/*
-rm $KERNELDIR/BUILT/zImage
+rm $KERNELDIR/BUILT_N7100/lib/modules/*
+rm $KERNELDIR/BUILT_N7100/zImage
 
-find -name '*.ko' -exec cp -av {} $KERNELDIR/BUILT/lib/modules/ \;
-${CROSS_COMPILE}strip --strip-unneeded $KERNELDIR/BUILT/lib/modules/*
-cp $KERNELDIR/arch/arm/boot/zImage $KERNELDIR/BUILT/
+find -name '*.ko' -exec cp -av {} $KERNELDIR/BUILT_N7100/lib/modules/ \;
+${CROSS_COMPILE}strip --strip-unneeded $KERNELDIR/BUILT_N7100/lib/modules/*
+cp $KERNELDIR/arch/arm/boot/zImage $KERNELDIR/BUILT_N7100/
 
 mv .git-halt .git
