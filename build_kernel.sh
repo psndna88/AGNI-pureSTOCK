888ab0c53fce67d7bc6c6e6fe5e0aa84db04ec94
psndna88 <psndna88@gmail.com>
2014-02-07 17:41:27 +0530

    Added v3.5.2 defconfig and updated build script (SELINUX PERMISSIVE)

--- build_kernel.sh
+++ build_kernel.sh
@@ -7,7 +7,7 @@ export ARCH=arm
 
 if [ ! -f $KERNELDIR/.config ];
 then
-  make defconfig psn_i9300_v3.5.1_defconfig
+  make defconfig psn_i9300_v3.5.2_defconfig
 fi
 
 . $KERNELDIR/.config
@@ -17,13 +17,13 @@ mv .git .git-halt
 cd $KERNELDIR/
 make -j2 || exit 1
 
-mkdir -p $KERNELDIR/BUILT/lib/modules
+mkdir -p $KERNELDIR/BUILT_I9300/lib/modules
 
-rm $KERNELDIR/BUILT/lib/modules/*
-rm $KERNELDIR/BUILT/zImage
+rm $KERNELDIR/BUILT_I9300/lib/modules/*
+rm $KERNELDIR/BUILT_I9300/zImage
 
-find -name '*.ko' -exec cp -av {} $KERNELDIR/BUILT/lib/modules/ \;
-${CROSS_COMPILE}strip --strip-unneeded $KERNELDIR/BUILT/lib/modules/*
-cp $KERNELDIR/arch/arm/boot/zImage $KERNELDIR/BUILT/
+find -name '*.ko' -exec cp -av {} $KERNELDIR/BUILT_I9300/lib/modules/ \;
+${CROSS_COMPILE}strip --strip-unneeded $KERNELDIR/BUILT_I9300/lib/modules/*
+cp $KERNELDIR/arch/arm/boot/zImage $KERNELDIR/BUILT_I9300/
 
 mv .git-halt .git
