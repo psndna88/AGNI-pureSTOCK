0f54cf82cf0a98bcc6f77747ad81d77d4c27d031
psndna88 <psndna88@gmail.com>
2014-05-05 12:07:43 +0530

    Revert "backport: smp: patches from mainline 3.5 to hopefully help with hotplug"
    This reverts commit 3aad0ef4ae9d46d5b1e6e096bfee7c35dfcc23ad.

--- kernel/sched.c
+++ kernel/sched.c
@@ -80,7 +80,6 @@
 #include "sched_cpupri.h"
 #include "workqueue_sched.h"
 #include "sched_autogroup.h"
-#include "smpboot.h"
 
 #define CREATE_TRACE_POINTS
 #include <trace/events/sched.h>
@@ -8263,7 +8262,6 @@ void __init sched_init(void)
 	/* May be allocated at isolcpus cmdline parse time */
 	if (cpu_isolated_map == NULL)
 		zalloc_cpumask_var(&cpu_isolated_map, GFP_NOWAIT);
-        idle_thread_set_boot_cpu();
 #endif /* SMP */
 
 	scheduler_running = 1;
