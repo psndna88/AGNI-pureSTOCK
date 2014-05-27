bdc64d0020d63fa9c857546829557c96640ee8f3
psndna88 <psndna88@gmail.com>
2014-05-27 12:58:43 +0530

    Revert Unwanted Stuff from "SAMSUNG OSRC SEA KK N7100XXUFND4 Partial Update"

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -3154,7 +3154,6 @@ long rtcc_reclaim_pages(long nr_to_reclaim)
 	struct task_struct *p = current;
 	unsigned long nr_reclaimed;
 
-	printk("RTCC, start reclaim!\n");
 	p->flags |= PF_MEMALLOC;
 	lockdep_set_current_reclaim_state(sc.gfp_mask);
 	reclaim_state.reclaimed_slab = 0;
