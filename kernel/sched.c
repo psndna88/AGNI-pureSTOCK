ecf4c80a87cef3b6ded524913fd323c16c1fdd79
psndna88 <psndna88@gmail.com>
2014-05-29 16:35:53 +0530

    Revert "kernel/sched.c : Revert changes from SAMSUNG OSRC SEA KK N7100XXUFND4 Partial Update"
    This reverts commit a0cfcf210a0275ceaadf55e841cb0b0390c93410.

--- kernel/sched.c
+++ kernel/sched.c
@@ -86,6 +86,8 @@
 #define CREATE_TRACE_POINTS
 #include <trace/events/sched.h>
 
+#include <linux/cpufreq_slp.h>
+
 ATOMIC_NOTIFIER_HEAD(migration_notifier_head);
 
 /*
@@ -4354,6 +4356,8 @@ need_resched:
 		rq->nr_switches++;
 		rq->curr = next;
 		++*switch_count;
+
+		slp_store_task_history(cpu, prev);
 
 		context_switch(rq, prev, next); /* unlocks the rq */
 		/*
