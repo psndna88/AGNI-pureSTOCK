6b7e91e61c60b6dfc308e0e426907c8dfe4be36b
psndna88 <psndna88@gmail.com>
2014-06-20 14:43:54 +0530

    Revert power-charging changes from "SAMSUNG OSRC SEA KK N7100XXUFND4 Update"

--- drivers/muic/max77693-muic.c
+++ drivers/muic/max77693-muic.c
@@ -1777,7 +1777,17 @@ static void max77693_otg_control(struct max77693_muic_info *info, int enable)
 			MAX77693_CHG_REG_CHG_CNFG_00, chg_cnfg_00);
 
 		mdelay(50);
-
+#if defined(CONFIG_MACH_T0_KOR_SKT) || defined(CONFIG_MACH_T0_KOR_KT) \
+		|| defined(CONFIG_MACH_T0_KOR_LGT)
+		/* [MAX77693] Workaround to get rid of reading dummy(0x00) */
+		/* disable charger detection again */
+		max77693_read_reg(info->max77693->muic,
+			MAX77693_MUIC_REG_CDETCTRL1, &cdetctrl1);
+		cdetctrl1 &= ~(1 << 0);
+		max77693_write_reg(info->max77693->muic,
+			MAX77693_MUIC_REG_CDETCTRL1, cdetctrl1);
+		mdelay(10);
+#endif
 		/* enable charger detection */
 		max77693_read_reg(info->max77693->muic,
 			MAX77693_MUIC_REG_CDETCTRL1, &cdetctrl1);
@@ -2711,44 +2721,21 @@ do {									\
 
 static int max77693_muic_irq_init(struct max77693_muic_info *info)
 {
+	int ret;
 	u8 val;
-	int ret = 0;
 
 	dev_info(info->dev, "%s: system_rev=%x\n", __func__, system_rev);
 
-	max77693_read_reg(info->muic, MAX77693_MUIC_REG_INT1, &val);
-	dev_info(info->dev, "%s: reg=%x, val=%x\n", __func__,
-		 MAX77693_MUIC_REG_INT1, val);
-	max77693_read_reg(info->muic, MAX77693_MUIC_REG_INT2, &val);
-	dev_info(info->dev, "%s: reg=%x, val=%x\n", __func__,
-		 MAX77693_MUIC_REG_INT2, val);
-	max77693_read_reg(info->muic, MAX77693_MUIC_REG_INT3, &val);
-	dev_info(info->dev, "%s: reg=%x, val=%x\n", __func__,
-		 MAX77693_MUIC_REG_INT3, val);
+	/* INTMASK1  3:ADC1K 2:ADCErr 1:ADCLow 0:ADC */
+	/* INTMASK2  0:Chgtype */
+	max77693_write_reg(info->muic, MAX77693_MUIC_REG_INTMASK1, 0x09);
+	max77693_write_reg(info->muic, MAX77693_MUIC_REG_INTMASK2, 0x11);
+	max77693_write_reg(info->muic, MAX77693_MUIC_REG_INTMASK3, 0x00);
 
-	if (info->max77693 && (info->max77693->irq_base > 0)) {
-		int irq_base = info->max77693->irq_base;
-
-		info->irq_adc = irq_base + MAX77693_MUIC_IRQ_INT1_ADC;
-		REQUEST_IRQ(info->irq_adc, "muic-adc");
-		if (ret < 0)
-			info->irq_adc = 0;
-
-		info->irq_chgtype = irq_base + MAX77693_MUIC_IRQ_INT2_CHGTYP;
-		REQUEST_IRQ(info->irq_chgtype, "muic-chgtype");
-		if (ret < 0)
-			info->irq_chgtype = 0;
-
-		info->irq_vbvolt = irq_base + MAX77693_MUIC_IRQ_INT2_VBVOLT;
-		REQUEST_IRQ(info->irq_vbvolt, "muic-vbvolt");
-		if (ret < 0)
-			info->irq_vbvolt = 0;
-
-		info->irq_adc1k = irq_base + MAX77693_MUIC_IRQ_INT1_ADC1K;
-		REQUEST_IRQ(info->irq_adc1k, "muic-adc1k");
-		if (ret < 0)
-			info->irq_adc1k = 0;
-	}
+	REQUEST_IRQ(info->irq_adc, "muic-adc");
+	REQUEST_IRQ(info->irq_chgtype, "muic-chgtype");
+	REQUEST_IRQ(info->irq_vbvolt, "muic-vbvolt");
+	REQUEST_IRQ(info->irq_adc1k, "muic-adc1k");
 
 	dev_info(info->dev, "adc:%d chgtype:%d adc1k:%d vbvolt:%d",
 		info->irq_adc, info->irq_chgtype,
@@ -2766,7 +2753,16 @@ static int max77693_muic_irq_init(struct max77693_muic_info *info)
 	dev_info(info->dev, "%s: reg=%x, val=%x\n", __func__,
 		 MAX77693_MUIC_REG_INTMASK3, val);
 
-	return ret;
+	max77693_read_reg(info->muic, MAX77693_MUIC_REG_INT1, &val);
+	dev_info(info->dev, "%s: reg=%x, val=%x\n", __func__,
+		 MAX77693_MUIC_REG_INT1, val);
+	max77693_read_reg(info->muic, MAX77693_MUIC_REG_INT2, &val);
+	dev_info(info->dev, "%s: reg=%x, val=%x\n", __func__,
+		 MAX77693_MUIC_REG_INT2, val);
+	max77693_read_reg(info->muic, MAX77693_MUIC_REG_INT3, &val);
+	dev_info(info->dev, "%s: reg=%x, val=%x\n", __func__,
+		 MAX77693_MUIC_REG_INT3, val);
+	return 0;
 }
 
 #define CHECK_GPIO(_gpio, _name)					\
@@ -3191,6 +3187,7 @@ static int __devinit max77693_muic_probe(struct platform_device *pdev)
 	struct max77693_muic_info *info;
 
 	int switch_sel;
+	u8 cntl1_val, cntl2_val, cntl3_val;
 	int ret;
 
 	pr_info("func:%s\n", __func__);
@@ -3204,6 +3201,10 @@ static int __devinit max77693_muic_probe(struct platform_device *pdev)
 	info->dev = &pdev->dev;
 	info->max77693 = max77693;
 	info->muic = max77693->muic;
+	info->irq_adc = max77693->irq_base + MAX77693_MUIC_IRQ_INT1_ADC;
+	info->irq_chgtype = max77693->irq_base + MAX77693_MUIC_IRQ_INT2_CHGTYP;
+	info->irq_vbvolt = max77693->irq_base + MAX77693_MUIC_IRQ_INT2_VBVOLT;
+	info->irq_adc1k = max77693->irq_base + MAX77693_MUIC_IRQ_INT1_ADC1K;
 	info->muic_data = pdata->muic;
 	info->is_adc_open_prev = true;
 	info->is_otg_test = false;
@@ -3291,9 +3292,20 @@ static int __devinit max77693_muic_probe(struct platform_device *pdev)
 
 	mutex_init(&info->mutex);
 
-	mutex_lock(&info->mutex);
-	max77693_muic_init_regs(info);
-	mutex_unlock(&info->mutex);
+	/* Set ADC debounce time: 25ms */
+	max77693_muic_set_adcdbset(info, 2);
+
+	max77693_read_reg(info->muic, MAX77693_MUIC_REG_CTRL1, &cntl1_val);
+	max77693_read_reg(info->muic, MAX77693_MUIC_REG_CTRL2, &cntl2_val);
+	max77693_read_reg(info->muic, MAX77693_MUIC_REG_CTRL3, &cntl3_val);
+	pr_info("%s:%s CONTROL[1:0x%x, 2:0x%x, 3:0x%x]\n", DEV_NAME, __func__,
+			cntl1_val, cntl2_val, cntl3_val);
+
+	ret = max77693_muic_irq_init(info);
+	if (ret < 0) {
+		dev_err(&pdev->dev, "Failed to initialize MUIC irq:%d\n", ret);
+		goto fail;
+	}
 
 	/* init jig state */
 	max77693_update_jig_state(info);
@@ -3327,6 +3339,14 @@ static int __devinit max77693_muic_probe(struct platform_device *pdev)
 	return 0;
 
  fail:
+	if (info->irq_adc)
+		free_irq(info->irq_adc, NULL);
+	if (info->irq_chgtype)
+		free_irq(info->irq_chgtype, NULL);
+	if (info->irq_vbvolt)
+		free_irq(info->irq_vbvolt, NULL);
+	if (info->irq_adc1k)
+		free_irq(info->irq_adc1k, NULL);
 	mutex_destroy(&info->mutex);
  err_input:
 	platform_set_drvdata(pdev, NULL);
@@ -3357,7 +3377,10 @@ static int __devexit max77693_muic_remove(struct platform_device *pdev)
 		cancel_delayed_work(&info->usb_work);
 		cancel_delayed_work(&info->mhl_work);
 
-		max77693_muic_free_irqs(info);
+		free_irq(info->irq_adc, info);
+		free_irq(info->irq_chgtype, info);
+		free_irq(info->irq_vbvolt, info);
+		free_irq(info->irq_adc1k, info);
 
 		wake_lock_destroy(&info->muic_wake_lock);
 		mutex_destroy(&info->mutex);
