76a3560422111d909f73e4d0587e15eadbc71e67
googyanas <googyanas@gmail.com>
2014-06-24 19:38:39 +0100

    ARM: smp: Wait just 1 second for other CPU to halt
    Currently, the busyloop waiting for a 2nd CPU to stop takes about 4
    seconds.  Adjust for the overhead of the loop by looping every 1ms
    instead of 1us.
    
    Signed-off-by: Chris Fries <C.Fries@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/537864
    SLT-Approved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Igor Kovalenko <cik009@motorola.com>
    Reviewed-by: Russell Knize <rknize2@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- arch/arm/kernel/smp.c
+++ arch/arm/kernel/smp.c
@@ -765,9 +765,9 @@ void smp_send_stop(void)
 	}
 
 	/* Wait up to one second for other CPUs to stop */
-	timeout = USEC_PER_SEC;
+	timeout = MSEC_PER_SEC;
 	while (num_online_cpus() > 1 && timeout--)
-		udelay(1);
+		mdelay(1);
 
 	if (num_online_cpus() > 1)
 		pr_warning("SMP: failed to stop secondary CPUs\n");
