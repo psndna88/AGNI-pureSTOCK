0908d3d7011820110a68b5a40158fb1d66e19988
psndna88 <psndna88@gmail.com>
2014-07-01 15:27:01 +0530

    Revert "f2fs: add a tracepoint for f2fs_write_end"
    This reverts commit 214d69c010ee1cd50f0d98bac8fb52a41d4e3be6.

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -991,9 +991,6 @@ static int f2fs_write_end(struct file *file,
 			struct page *page, void *fsdata)
 {
 	struct inode *inode = page->mapping->host;
-
-	trace_f2fs_write_end(inode, pos, len, copied);
-
 	SetPageUptodate(page);
 	set_page_dirty(page);
 
