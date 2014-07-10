996fd156d72359628a7d694fffbd76eb8bd58c85
psndna88 <psndna88@gmail.com>
2014-07-10 10:40:06 +0530

    PM: Fix missing dependencies

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -159,12 +159,13 @@ void __attribute__ ((weak)) arch_suspend_enable_irqs(void)
 }
 
 /**
- *	suspend_enter - enter the desired system sleep state.
+ * suspend_enter - enter the desired system sleep state.
  *	@state:		state to enter
+ *  @wakeup: Returns information that suspend should not be entered again.
  *
- *	This function should be called after devices have been suspended.
+ * This function should be called after devices have been suspended.
  */
-static int suspend_enter(suspend_state_t state)
+static int suspend_enter(suspend_state_t state, bool *wakeup)
 {
 	int error;
 
