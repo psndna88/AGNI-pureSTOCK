c5465adfcb9fbeec965506783e293c6b0ecdf7ee
psndna88 <psndna88@gmail.com>
2014-08-09 21:46:27 +0530

    cpufreq: bring-up intelliactive (arter97)
    This one's from my S4 LTE-A Git, slightly tweaked one.
    
    http://bitbucket.org/arter97/android_kernel_samsung_ks01lte

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -384,6 +384,9 @@ extern struct cpufreq_governor cpufreq_gov_adaptive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PEGASUSQ)
 extern struct cpufreq_governor cpufreq_gov_pegasusq;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_pegasusq)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LULZACTIVEQ)
+extern struct cpufreq_governor cpufreq_gov_lulzactiveq;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_lulzactiveq)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_YANKASUSQ)
 extern struct cpufreq_governor cpufreq_gov_yankasusq;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_yankasusq)
