12fde0aaed9e11a45afd1c81a99b2100607f0142
psndna88 <psndna88@gmail.com>
2014-09-21 12:40:39 +0530

    Update v4.7 configs

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 export KERNELDIR=`readlink -f .`
 . ~/AGNi_stamp_STOCK.sh
-. ~/gcc_4.9.1_linaro_cortex-a9.sh
+. ~/gcc_4.9.2_linaro_cortex-a9.sh
 
 export ARCH=arm
 
 if [ ! -f $KERNELDIR/.config ];
 then
-  make defconfig psn_i9300_v4.6_defconfig
+  make defconfig psn_i9300_v4.7_defconfig
 fi
 
 . $KERNELDIR/.config
