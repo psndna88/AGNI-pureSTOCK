e1d0450c428deb1a30370f5d4de5d9f92d8b19e3
psndna88 <psndna88@gmail.com>
2014-10-23 20:37:36 +0530

    Implement USB HID Keyboard/Mouse ROLE support for Android device https://github.com/pelya/android-keyboard-gadget
    needs supported APK from :
    https://raw.githubusercontent.com/pelya/android-keyboard-gadget/master/USB-Keyboard.apk

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -59,6 +59,9 @@
 #include "f_mtp.c"
 #endif
 #include "f_accessory.c"
+#include "f_hid.h"
+#include "f_hid_android_keyboard.c"
+#include "f_hid_android_mouse.c"
 #define USB_ETH_RNDIS y
 #include "f_rndis.c"
 #include "rndis.c"
@@ -1159,6 +1162,41 @@ static struct android_usb_function audio_source_function = {
 	.attributes	= audio_source_function_attributes,
 };
 
+static int hid_function_init(struct android_usb_function *f, struct usb_composite_dev *cdev)
+{
+	return ghid_setup(cdev->gadget, 2);
+}
+
+static void hid_function_cleanup(struct android_usb_function *f)
+{
+	ghid_cleanup();
+}
+
+static int hid_function_bind_config(struct android_usb_function *f, struct usb_configuration *c)
+{
+	int ret;
+	printk(KERN_INFO "hid keyboard\n");
+	ret = hidg_bind_config(c, &ghid_device_android_keyboard, 0);
+	if (ret) {
+		pr_info("%s: hid_function_bind_config keyboard failed: %d\n", __func__, ret);
+		return ret;
+	}
+	printk(KERN_INFO "hid mouse\n");
+	ret = hidg_bind_config(c, &ghid_device_android_mouse, 1);
+	if (ret) {
+		pr_info("%s: hid_function_bind_config mouse failed: %d\n", __func__, ret);
+		return ret;
+	}
+	return 0;
+}
+
+static struct android_usb_function hid_function = {
+	.name		= "hid",
+	.init		= hid_function_init,
+	.cleanup	= hid_function_cleanup,
+	.bind_config	= hid_function_bind_config,
+};
+
 static struct android_usb_function *supported_functions[] = {
 	&adb_function,
 	&acm_function,
@@ -1176,6 +1214,7 @@ static struct android_usb_function *supported_functions[] = {
 	&diag_function,
 	&dm_function,
 	&audio_source_function,
+	&hid_function,
 	NULL
 };
 
@@ -1377,6 +1416,9 @@ functions_store(struct device *pdev, struct device_attribute *attr,
 #endif
 		}
 	}
+
+	/* HID driver always enabled, it's the whole point of this kernel patch */
+	android_enable_function(dev, "hid");
 
 	mutex_unlock(&dev->mutex);
 
