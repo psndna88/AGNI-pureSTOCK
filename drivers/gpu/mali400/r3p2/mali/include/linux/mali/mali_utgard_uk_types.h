1c3542285b1c9e50b268e5d8e4b411fa55c201d9
psndna88 <psndna88@gmail.com>
2014-10-29 08:04:22 +0530

    Set _MALI_API_VERSION as "29" to support newer 4.4.4 mali blobs

--- drivers/gpu/mali400/r3p2/mali/include/linux/mali/mali_utgard_uk_types.h
+++ drivers/gpu/mali400/r3p2/mali/include/linux/mali/mali_utgard_uk_types.h
@@ -723,16 +723,7 @@ typedef struct
  * The 16bit integer is stored twice in a 32bit integer
  * For example, for version 1 the value would be 0x00010001
  */
-#define _MALI_API_VERSION 26
-
-/**
- * HACK: We must keep API version at the same level as binary blob
- * Damn you, Samsung!
- */
-#undef _MALI_API_VERSION
-//#define _MALI_API_VERSION 29 /* I9305 EUR KK update */
-#define _MALI_API_VERSION 23
-
+#define _MALI_API_VERSION 29
 #define _MALI_UK_API_VERSION _MAKE_VERSION_ID(_MALI_API_VERSION)
 
 /**
