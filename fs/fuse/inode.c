3375e6a68404ab5c004a6be864461b917c481556
psndna88 <psndna88@gmail.com>
2014-11-22 16:33:18 +0530

    Revert "make default readahead size a kernel parameter"

--- fs/fuse/inode.c
+++ fs/fuse/inode.c
@@ -894,7 +894,8 @@ static int fuse_bdi_init(struct fuse_conn *fc, struct super_block *sb)
 	int err;
 
 	fc->bdi.name = "fuse";
-	fc->bdi.ra_pages = max_readahead_pages;
+	fc->bdi.ra_pages = (VM_MAX_READAHEAD * 1024) / PAGE_CACHE_SIZE;
+	fc->bdi.ra_pages *= 4;
 	/* fuse does it's own writeback accounting */
 	fc->bdi.capabilities = BDI_CAP_NO_ACCT_WB;
 
