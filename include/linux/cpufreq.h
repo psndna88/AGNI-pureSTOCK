c32595199e3f96aab9fa53f539bc24d3d5d7bf1e
psndna88 <psndna88@gmail.com>
2014-11-22 15:09:15 +0530

    intelliactive: backport to Linux 3.0.x / Linux 3.1.x kernels cpufreq: intelliactive: initial coding and introduction!
    intelliactive is based on Latest Google Interactive cpufreq governor from
    Google.  The original Interactive provides smooth UI operations but suffers
    slight battery drain due to its aggressive policies.  Intelliactive is an
    improvement built on top of it.
    
    It has the following enhancements added:
    1. self-boost capability from input drivers (no need for PowerHAL assist)
    2. two phase scheduling (idle/busy phases to prevent from jumping directly to
       max freq
    3. Checks for offline cpus and short circuits some unnecessary checks to
       improve code execution paths
    4. removed trace calls
    
    All credits to Faux123
    
    intelliactive: fix race condition when unregistering input handlers
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    fix intelliactive and timer

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -358,12 +358,21 @@ extern struct cpufreq_governor cpufreq_gov_ondemand;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CONSERVATIVE)
 extern struct cpufreq_governor cpufreq_gov_conservative;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_conservative)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
+extern struct cpufreq_governor cpufreq_gov_zzmoove;
+#define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_zzmoove)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HOTPLUG)
 extern struct cpufreq_governor cpufreq_gov_hotplug;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_hotplug)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_YANKASUSQ)
+extern struct cpufreq_governor cpufreq_gov_yankasusq;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_yankasusq)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
+extern struct cpufreq_governor cpufreq_gov_intelliactive;
+#define CPUFREQ_DEFAULT_GOVERNOR    (&cpufreq_gov_intelliactive)
 #endif
 
 
