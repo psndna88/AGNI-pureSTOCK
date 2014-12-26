ac42ec1159085cc9cbbf15476810db5c091146c7
psndna88 <psndna88@gmail.com>
2014-12-26 17:17:35 +0530

    Add v5.4 configs

---
+++ build_kernel_wolf.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+export KERNELDIR=`readlink -f .`
+. ~/AGNi_stamp_STOCK.sh
+. ~/gcc_4.9.3_linaro_cortex-a9.sh
+
+export ARCH=arm
+
+if [ ! -f $KERNELDIR/.config ];
+then
+  make defconfig psn_i9305_new_wolf_defconfig
+fi
+
+. $KERNELDIR/.config
+
+mv .git .git-halt
+
+cd $KERNELDIR/
+make -j2 || exit 1
+
+mkdir -p $KERNELDIR/BUILT_I9305_wolf/lib/modules
+
+rm $KERNELDIR/BUILT_I9305_wolf/lib/modules/*
+rm $KERNELDIR/BUILT_I9305_wolf/zImage
+
+find -name '*.ko' -exec cp -av {} $KERNELDIR/BUILT_I9305_wolf/lib/modules/ \;
+${CROSS_COMPILE}strip --strip-unneeded $KERNELDIR/BUILT_I9305_wolf/lib/modules/*
+cp $KERNELDIR/arch/arm/boot/zImage $KERNELDIR/BUILT_I9305_wolf/
+
+mv .git-halt .git
