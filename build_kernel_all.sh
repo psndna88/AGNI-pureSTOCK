44d33ece349f86cfe9b70aa646703c320b46d731
psndna88 <psndna88@gmail.com>
2015-01-17 21:46:31 +0530

    Merge Branch "pureSTOCK-I605-4.4.4"

---
+++ build_kernel_all.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+export KERNELDIR=`readlink -f .`
+
+cd $KERNELDIR;
+
+echo "Building N7100 .....";
+./build_kernel_n7100.sh && sleep 10 && rm .config;
+
+echo "Building N7105 .....";
+./build_kernel_n7105.sh && sleep 10 && rm .config;
+
+echo "Building I605 .....";
+./build_kernel_i605.sh && sleep 10 && rm .config;
+
---
+++ build_kernel_i605.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+export KERNELDIR=`readlink -f .`
+. ~/AGNi_stamp_STOCK.sh
+. ~/gcc_4.9.3_linaro_cortex-a9.sh
+
+export ARCH=arm
+
+if [ ! -f $KERNELDIR/.config ];
+then
+  make defconfig psn_t0ltevzw_new_defconfig
+fi
+
+. $KERNELDIR/.config
+
+mv .git .git-halt
+
+cd $KERNELDIR/
+make -j2 || exit 1
+
+mkdir -p $KERNELDIR/BUILT_I605/lib/modules
+
+rm $KERNELDIR/BUILT_I605/lib/modules/*
+rm $KERNELDIR/BUILT_I605/zImage
+
+find -name '*.ko' -exec cp -av {} $KERNELDIR/BUILT_I605/lib/modules/ \;
+${CROSS_COMPILE}strip --strip-unneeded $KERNELDIR/BUILT_I605/lib/modules/*
+cp $KERNELDIR/arch/arm/boot/zImage $KERNELDIR/BUILT_I605/
+
+mv .git-halt .git
