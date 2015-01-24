6e3651b476ff1e164641c5dfaecdaef54cae7dd9
psndna88 <psndna88@gmail.com>
2015-01-24 13:12:39 +0530

    update build kernel all script

--- build_kernel_all.sh
+++ build_kernel_all.sh
@@ -6,17 +6,17 @@ cd $KERNELDIR;
 echo "Building N7100 .....";
 ./build_kernel_n7100.sh && sleep 10 && rm .config;
 
-echo "Building N7105 .....";
-./build_kernel_n7105.sh && sleep 10 && rm .config;
-
-echo "Building I605 .....";
-./build_kernel_i605.sh && sleep 10 && rm .config;
-
 echo "Building N7100 CM .....";
 ./build_kernel_CM_n7100.sh && sleep 10 && rm .config;
 
+echo "Building N7105 .....";
+./build_kernel_n7105.sh && sleep 10 && rm .config;
+
 echo "Building N7105 CM .....";
 ./build_kernel_CM_n7105.sh && sleep 10 && rm .config;
+
+echo "Building I605 .....";
+./build_kernel_i605.sh && sleep 10 && rm .config;
 
 echo "Building I605 CM .....";
 ./build_kernel_CM_i605.sh && sleep 10 && rm .config;
